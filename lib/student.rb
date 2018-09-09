f7f2e4a8818a143687f7ea8031a9df031c5f7327
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-09 23:31:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,7 @@ class Student
   end
 
   def self.new_from_db(row)
+    #converts what the database gives us into a ruby object
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
@@ -49,5 +50,5 @@ class Student
     new_student
   end
 
-  
+
 end
