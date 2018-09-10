9d0087d730c8ddf07840ade1df1f496ee0ff394d
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:49:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
   def first_X_students_in_grade_10
     sql = <<-SQL
     SELECT *
-    FROM students WHERE grade = 10 
+    FROM students WHERE grade = 10 LIMIT ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
