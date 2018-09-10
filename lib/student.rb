6b1a71a39111f9c201b2eafe23cef16e22788f09
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 01:27:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
   def self.all_students_in_grade_X
     sql = <<-SQL
     SELECT *
-    FROM students WHERE grade = 9
+    FROM students WHERE grade = x
     SQL
 
     DB[:conn].execute(sql).map do |row|
