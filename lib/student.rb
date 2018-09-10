04dbd400e505439fc7a56317745bb0cb29bcabfc
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:15:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,17 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_below_grade_12
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
