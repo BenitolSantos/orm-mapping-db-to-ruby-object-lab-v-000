05057fea4d4265b83cd3a470f1700a31534578d9
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:16:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.all_students_below_grade_12
+  def self.all_students_below_12th_grade
     sql = <<-SQL
     SELECT *
     FROM students WHERE grade < 12
