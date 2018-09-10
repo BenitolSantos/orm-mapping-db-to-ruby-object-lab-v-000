88d2c4fcde179e37196485c5d616a003f86bf2a9
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:26:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     FROM students WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name, grade).map do |row|
       self.new_from_db(row)
     end.first
   end
