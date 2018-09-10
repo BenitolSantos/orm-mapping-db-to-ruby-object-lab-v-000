e00981fa00ffe740b4c1c166f7d5f870cdec0c07
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:40:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
     FROM students
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.all_students_in_grade_9
