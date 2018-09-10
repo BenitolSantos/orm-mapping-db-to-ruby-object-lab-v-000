eee41dad94b4b1948b61d927f85170ed2917a3cc
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 00:15:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,8 +33,6 @@ class Student
   end
 
   def self.all_students_below_grade_12
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
     sql = <<-SQL
     SELECT *
     FROM students WHERE grade < 12
